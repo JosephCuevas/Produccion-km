84297b2d5ee4d6f904776a73d6389407d57fce6f
Joseph <chequecuevas@gmail.com>
2020-09-05 13:17:47 -0500

    reestructura de la base de datos

--- proceso_produccion.sql
+++ proceso_produccion.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-08-2020 a las 04:02:10
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.3
+-- Tiempo de generación: 04-09-2020 a las 22:41:33
+-- Versión del servidor: 10.4.10-MariaDB
+-- Versión de PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,20 +25,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cantidad`
+--
+
+CREATE TABLE `cantidad` (
+  `id_cantidad` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `cliente`
 --
 
 CREATE TABLE `cliente` (
   `id_cliente` int(11) NOT NULL,
-  `nombre` varchar(250) NOT NULL
+  `nombre` varchar(250) NOT NULL,
+  `apellido_pat` varchar(250) NOT NULL,
+  `apellido_mat` varchar(250) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `cliente`
 --
 
-INSERT INTO `cliente` (`id_cliente`, `nombre`) VALUES
-(1, 'Joseff');
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `apellido_pat`, `apellido_mat`, `fecha`) VALUES
+(3, 'josue', 'cuevas', 'sanchez', '2020-09-04 20:19:12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compra`
+--
+
+CREATE TABLE `compra` (
+  `id_compra` int(11) NOT NULL,
+  `compra` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `diseno`
+--
+
+CREATE TABLE `diseno` (
+  `id_diseno` int(11) NOT NULL,
+  `diseno` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -47,19 +83,25 @@ INSERT INTO `cliente` (`id_cliente`, `nombre`) VALUES
 --
 
 CREATE TABLE `disposicion` (
+  `id_disposicion` int(11) NOT NULL,
   `id_orden` int(11) NOT NULL,
-  `nombre` varchar(250) NOT NULL,
-  `compra` varchar(250) NOT NULL,
-  `proceso` varchar(250) NOT NULL,
-  `estado` varchar(250) NOT NULL
+  `nombre` int(11) NOT NULL,
+  `compra` int(11) NOT NULL,
+  `proceso` int(11) NOT NULL,
+  `estado` int(11) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `disposicion`
+-- Estructura de tabla para la tabla `estado`
 --
 
-INSERT INTO `disposicion` (`id_orden`, `nombre`, `compra`, `proceso`, `estado`) VALUES
-(1, 'Joseff', 'Website', 'Almacen', 'En espera');
+CREATE TABLE `estado` (
+  `id_estado` int(11) NOT NULL,
+  `estado` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -69,23 +111,46 @@ INSERT INTO `disposicion` (`id_orden`, `nombre`, `compra`, `proceso`, `estado`) 
 
 CREATE TABLE `orden` (
   `id_orden` int(11) NOT NULL,
-  `nombre` varchar(250) NOT NULL,
-  `diseno` varchar(250) NOT NULL,
-  `tipo_prenda` varchar(250) NOT NULL,
-  `cantidad` int(4) NOT NULL,
-  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `nombre` int(11) NOT NULL,
+  `diseno` int(11) NOT NULL,
+  `tipo_prenda` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `compra` int(11) NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Volcado de datos para la tabla `orden`
+-- Estructura de tabla para la tabla `proceso`
 --
 
-INSERT INTO `orden` (`id_orden`, `nombre`, `diseno`, `tipo_prenda`, `cantidad`, `fecha`) VALUES
-(1, 'Joseff', 'Cthulhu', 'Playera', 1, '2020-08-28 00:14:10');
+CREATE TABLE `proceso` (
+  `id_proceso` int(11) NOT NULL,
+  `proceso` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_prenda`
+--
+
+CREATE TABLE `tipo_prenda` (
+  `id_prenda` int(11) NOT NULL,
+  `tipo_prenda` varchar(250) NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `cantidad`
+--
+ALTER TABLE `cantidad`
+  ADD PRIMARY KEY (`id_cantidad`);
 
 --
 -- Indices de la tabla `cliente`
@@ -94,26 +159,145 @@ ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id_cliente`);
 
 --
+-- Indices de la tabla `compra`
+--
+ALTER TABLE `compra`
+  ADD PRIMARY KEY (`id_compra`);
+
+--
+-- Indices de la tabla `diseno`
+--
+ALTER TABLE `diseno`
+  ADD PRIMARY KEY (`id_diseno`);
+
+--
+-- Indices de la tabla `disposicion`
+--
+ALTER TABLE `disposicion`
+  ADD PRIMARY KEY (`id_disposicion`),
+  ADD UNIQUE KEY `id_orden` (`id_orden`),
+  ADD UNIQUE KEY `nombre` (`nombre`),
+  ADD UNIQUE KEY `compra` (`compra`),
+  ADD UNIQUE KEY `proceso` (`proceso`),
+  ADD UNIQUE KEY `estado` (`estado`);
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id_estado`);
+
+--
 -- Indices de la tabla `orden`
 --
 ALTER TABLE `orden`
-  ADD PRIMARY KEY (`id_orden`);
+  ADD PRIMARY KEY (`id_orden`),
+  ADD UNIQUE KEY `nombre` (`nombre`),
+  ADD UNIQUE KEY `diseno` (`diseno`),
+  ADD UNIQUE KEY `tipo_prenda` (`tipo_prenda`),
+  ADD UNIQUE KEY `tipo_prenda_2` (`tipo_prenda`),
+  ADD UNIQUE KEY `compra` (`compra`),
+  ADD UNIQUE KEY `cantidad` (`cantidad`);
+
+--
+-- Indices de la tabla `proceso`
+--
+ALTER TABLE `proceso`
+  ADD PRIMARY KEY (`id_proceso`);
+
+--
+-- Indices de la tabla `tipo_prenda`
+--
+ALTER TABLE `tipo_prenda`
+  ADD PRIMARY KEY (`id_prenda`),
+  ADD UNIQUE KEY `cantidad` (`cantidad`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cantidad`
+--
+ALTER TABLE `cantidad`
+  MODIFY `id_cantidad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `compra`
+--
+ALTER TABLE `compra`
+  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `diseno`
+--
+ALTER TABLE `diseno`
+  MODIFY `id_diseno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `disposicion`
+--
+ALTER TABLE `disposicion`
+  MODIFY `id_disposicion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `orden`
 --
 ALTER TABLE `orden`
-  MODIFY `id_orden` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_orden` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `proceso`
+--
+ALTER TABLE `proceso`
+  MODIFY `id_proceso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_prenda`
+--
+ALTER TABLE `tipo_prenda`
+  MODIFY `id_prenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `disposicion`
+--
+ALTER TABLE `disposicion`
+  ADD CONSTRAINT `disposicion_ibfk_1` FOREIGN KEY (`nombre`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disposicion_ibfk_2` FOREIGN KEY (`id_orden`) REFERENCES `orden` (`id_orden`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disposicion_ibfk_3` FOREIGN KEY (`estado`) REFERENCES `estado` (`id_estado`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `disposicion_ibfk_4` FOREIGN KEY (`proceso`) REFERENCES `proceso` (`id_proceso`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `orden`
+--
+ALTER TABLE `orden`
+  ADD CONSTRAINT `orden_ibfk_1` FOREIGN KEY (`nombre`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_2` FOREIGN KEY (`diseno`) REFERENCES `diseno` (`id_diseno`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_3` FOREIGN KEY (`tipo_prenda`) REFERENCES `tipo_prenda` (`id_prenda`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_4` FOREIGN KEY (`compra`) REFERENCES `compra` (`id_compra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orden_ibfk_5` FOREIGN KEY (`cantidad`) REFERENCES `cantidad` (`id_cantidad`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tipo_prenda`
+--
+ALTER TABLE `tipo_prenda`
+  ADD CONSTRAINT `tipo_prenda_ibfk_1` FOREIGN KEY (`cantidad`) REFERENCES `cantidad` (`id_cantidad`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
